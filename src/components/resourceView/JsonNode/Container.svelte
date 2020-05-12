2820aaab6ac5123ac3e520bf1b82b3679c987021
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-05-12 17:29:21 -0400

    Update Container.svelte

--- src/components/resourceView/JsonNode/Container.svelte
+++ src/components/resourceView/JsonNode/Container.svelte
@@ -35,18 +35,16 @@
   let element;
   let timeout;
   let firstLine = 0;
-  let lastLine = Infinity;
+  let lastLine = 125;
   async function redraw() {
     if (treeNode.length < 125) return;
     if (element) {
       const { top, bottom } = element.getBoundingClientRect();
-      if (top - 85 < -24 * 50) {
-        firstLine = firstLine - 50 + (top - 57) / -24;
-      } else {
-        firstLine = 0;
-      }
+      const calculated = firstLine - 50 + (top - 57) / -24;
+      firstLine = calculated >= 0 ? calculated : 0;
       lastLine = firstLine + 100 + (window.innerHeight) / 24;
     }
+    console.log(firstLine, lastLine)
     timeout = setTimeout(redraw, 1000);
   }
   onMount(redraw);
