85bde58c113db0eb47ce497e6a4b29535d0473a6
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-07-14 19:05:36 -0400

    Update Container.svelte

--- src/components/resourceView/JsonNode/Container.svelte
+++ src/components/resourceView/JsonNode/Container.svelte
@@ -33,25 +33,6 @@
   import Arrow from "../../icons/Arrow.svelte";
   // transformations
   import { expandAndFade } from "../../../utils/transition.js";
-
-  let element;
-  /*
-  let timeout;
-  let firstLine = 0;
-  let lastLine = 125;
-  async function redraw() {
-    if (treeNode.length < 125) return;
-    if (element) {
-      const { top, bottom } = element.getBoundingClientRect();
-      const calculated = firstLine - 50 + (top - 57) / -24;
-      firstLine = calculated >= 0 ? calculated : 0;
-      lastLine = firstLine + 100 + (window.innerHeight) / 24;
-    }
-    console.log(firstLine, lastLine)
-    timeout = setTimeout(redraw, 700);
-  }
-  onMount(redraw);
-  */
 </script>
 
 <style>
@@ -59,12 +40,6 @@
     contain: content;
   }
 </style>
-
-<!--
-{#if firstLine > 0}
-  <div style="height: {firstLine * 24}px" />
-{/if}
--->
 
 {#if expanded && diff && diff.position === 'before'}
   <svelte:component
@@ -98,7 +73,6 @@
 {#if expanded}
   <div
     transition:expandAndFade|local
-    bind:this={element}
     style="height: {firstLevel ? 'calc(100vh - 89px)' : `${treeNode.length * 24}px`}"
     class={hover ? 'bg-gray-300' : ''}>
     <VirtualList items={treeNode} let:index let:item>
@@ -113,20 +87,6 @@
           on:loadDeeper />
       </div>
     </VirtualList>
-    <!--{#each treeNode as n, index}
-      {#if index >= firstLine && index <= lastLine}
-        <div
-          on:mouseover|stopPropagation={() => (hover = true)}
-          on:mouseout|stopPropagation={() => (hover = false)}
-          class="pl-4">
-          <svelte:component
-            this={n.component}
-            props={n.props}
-            {index}
-            on:loadDeeper />
-        </div>
-      {/if}
-    {/each}-->
   </div>
 {/if}
 
