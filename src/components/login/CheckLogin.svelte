e6d71b2bf6f6b0dc27753749d0ca9cc1735b64f5
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-07-29 13:34:27 -0400

    Update CheckLogin.svelte

--- src/components/login/CheckLogin.svelte
+++ src/components/login/CheckLogin.svelte
@@ -2,11 +2,13 @@
   // whenever logged out, redirect to login
   import { goto } from "@sapper/app";
   import { loggedIn } from "../../store";
-  import { onMount } from "svelte";
 
-  onMount(() => {
+  const checkLogin = () => {
     if (!$loggedIn) {
       goto("./login");
     }
-  });
+  }
+  if (typeof window !== 'undefined') {
+    checkLogin();
+  }
 </script>
